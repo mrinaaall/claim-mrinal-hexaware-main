18749224cd46bea71e8fd4f93f2ba13ec813f8c6
Mrinal Soni <51278108+mrinaaall@users.noreply.github.com>
2022-08-23 12:43:34 -0400

    Update redirect.js

--- src/main/resources/static/redirect.js
+++ src/main/resources/static/redirect.js
@@ -1,4 +1,3 @@
-// ToDo: Write code to redirect this back to LogIn page upon successful submission of details.
 const registerButton = document.getElementById("register-submit");
 const registerForm = document.getElementById("login-form");
 
@@ -9,22 +8,25 @@ registerButton.addEventListener("click", (e) => {
     const firstName = registerForm.firstname.value;
     const lastName = registerForm.lastname.value;
     const email = registerForm.email.value;
+    // const email = document.getElementById("email");
 
-    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;  //Javascript reGex for Email Validation.
-    var regName = /\d+$/g;                                    // Javascript reGex for Name validation
+    //Javascript reGex for Email Validation
+    // var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w {2, 3})+$/;
+    var regName = /\d+$/g;
 
+    // Providing alerts for invalid entries made by the user
     if (firstName == null || firstName === "" || regName.test(firstName)) {
         window.alert('Please enter valid first name !');
     } else if (lastName == null || lastName === "") {
         window.alert('Please enter valid last name !');
-    } else if (email == null || email === "" ) {
+    } else if (email == null || email === "" || email.match(/.*\@.*\.\w{2,3}/g) === null) { // RegEx to validate email format
         window.alert('Please enter valid email !');
     } else if (username == null || username === "") {
         window.alert('Please enter valid user name !');
     } else if (password == null || password === "") {
         window.alert('Please enter valid password !');
-    } else {
-
+    } 
+    else {
         const body = {
             "firstName": firstName,
             "lastName": lastName,
@@ -42,12 +44,10 @@ registerButton.addEventListener("click", (e) => {
         }).then(res => res.json())
             .then(data => {
                 console.log('Data', data);
-                location.href = "index.html";
+                location.href = "\index.html"; // Redirect to LogIn screen after a successful registration
         }).catch(err=>{
             window.alert("Username already exists !")
-            console.log('err', err);
+            console.log('err', err); // Provide an error if user already exists
         })
-
-
     }
 })
